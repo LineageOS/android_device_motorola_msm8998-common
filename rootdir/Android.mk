d0a2cf7f1bf76cde2e1912c85cfddf1c3bdd3fb1
Nolen Johnson <johnsonnolen@gmail.com>
2022-08-09 10:54:48 -0400

    msm8998-common: Swap to QTI USB init scripts
    Change-Id: Ibb01a6a8b9817b01a8db397cdf82a89659562d62

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -51,15 +51,6 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mmi.usb.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mmi.usb.rc
-LOCAL_VENDOR_MODULE    := true
-LOCAL_MODULE_RELATIVE_PATH := init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.class_main.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
