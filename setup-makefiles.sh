8bc15000719513c51da85a53ef15896a365b19c2
Georg Veichtlbauer <georg@vware.at>
2023-04-15 19:29:33 +0200

    msm8998-common: Add support for nash
    Change-Id: If85e973faddf17c1b5faee2261461f0e0029db31

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "beckham chef evert lake payton"
+write_headers "beckham chef evert lake payton nash"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
