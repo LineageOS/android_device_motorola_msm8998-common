9349b9374cbe0720f89369bc403a8fb62e91bb57
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-18 19:48:28 -0500

    msm8998-common: Rework OEM/Vendor fstab split handling
    * Module stems screw with things and make it so that all devices get
      the last declared module (being OEM version).
    * Guard in Android.mk instead with BoardFlag for affected devices.
    
    Change-Id: I7a5761c0e7cbc8e47eed54ab87c5015859b32d36

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,6 +3,7 @@ include $(CLEAR_VARS)
 
 # Device init scripts
 
+ifneq ($(TARGET_USES_OEM_AS_VENDOR),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
@@ -20,25 +21,25 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/first_stage_ramdisk
 include $(BUILD_PREBUILT)
-
+else
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom.oem
-LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom.oem
-LOCAL_REQUIRED_MODULES := fstab.qcom.oem_ramdisk
+LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk
 LOCAL_VENDOR_MODULE    := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom.oem_ramdisk
+LOCAL_MODULE       := fstab.qcom_ramdisk
 LOCAL_MODULE_STEM  := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom.oem
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/first_stage_ramdisk
 include $(BUILD_PREBUILT)
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mmi.ramdump.rc
