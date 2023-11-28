0e142a73a39453e4702bd347c0b78d8462c4e75d
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-27 22:05:17 -0500

    msm8998-common: Alphabetize vendor SPL
    Change-Id: I78fefe0894f044c9c7e890d345b57d8d3eb821c9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,9 +133,6 @@ ODM_MANIFEST_QCRIL_FILES := $(PLATFORM_PATH)/odm_manifest_qcril.xml
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /mnt/vendor/persist:/persist
 
-# Vendor Security Patch Level
-VENDOR_SECURITY_PATCH := 2021-08-01
-
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
@@ -150,6 +147,9 @@ endif
 # Treble
 BOARD_VNDK_VERSION := current
 PRODUCT_FULL_TREBLE_OVERRIDE := true
+
+# Vendor Security Patch Level
+VENDOR_SECURITY_PATCH := 2021-08-01
 
 # Verified Boot
 BOARD_AVB_ENABLE := false
