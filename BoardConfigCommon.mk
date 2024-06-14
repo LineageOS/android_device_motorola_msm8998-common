d2a2ce25c932c5c4862434a3e95d3d2c70000210
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:46:19 +0300

    msm8998-common: Include lineage FCM
    Change-Id: Ib4408e941d920a3a38132ba1c8360994f5534c0b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,7 +61,8 @@ LOC_HIDL_VERSION := 3.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest.xml
 ifdef BOARD_USES_KEYMASTER_4
