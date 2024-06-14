f106d3bb678a7560aabb7213182499bcc1fa848d
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:46:19 +0300

    msm8998-common: Include lineage FCM
    Change-Id: Ib4408e941d920a3a38132ba1c8360994f5534c0b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,7 +54,8 @@ endif
 DEVICE_MATRIX_FILE := $(PLATFORM_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := \
     $(PLATFORM_PATH)/framework_manifest.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 TARGET_FS_CONFIG_GEN += \
     $(PLATFORM_PATH)/config.fs \
