79212008ba7deaec5864dd2b31daa40621219861
Nolen Johnson <johnsonnolen@gmail.com>
2024-12-01 20:01:36 -0500

    msm8998-common: Copy libutils-v32:32 in common tree
    Change-Id: Ife119a1befb4c9a71f3d267152e9253c5b994e0c

--- common.mk
+++ common.mk
@@ -407,6 +407,7 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
 
 # Update this list with what each blob is actually for
