0049bcc4bf0ac98f8b7cd43a153dde7eaf9b039a
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 04:12:35 +0200

    msm8998-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Idb002fc81fca3321f70fb3bba83f4af44354ce81

--- common.mk
+++ common.mk
@@ -103,11 +103,16 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
+<<<<<<< PATCH SET (50ed1f msm8998-common: Update display flags & packages)
+    gralloc.qcom \
+    hwcomposer.qcom \
+=======
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.$(TARGET_MOTO_PLATFORM) \
     hwcomposer.$(TARGET_MOTO_PLATFORM) \
     memtrack.$(TARGET_MOTO_PLATFORM) \
+>>>>>>> BASE      (8d643a msm8998-common: TARGET_PROVIDES_LIBLIGHT is dead)
     libdisplayconfig \
     libqdMetaData.system \
     libvulkan \
