3f571c547f2fce8b80022235cefcd1da8d96ed7c
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 04:12:35 +0200

    msm8998-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Idb002fc81fca3321f70fb3bba83f4af44354ce81

--- common.mk
+++ common.mk
@@ -104,7 +104,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     gralloc.$(TARGET_MOTO_PLATFORM) \
-    hwcomposer.$(TARGET_MOTO_PLATFORM) \
+    hwcomposer.qcom \
     libdisplayconfig \
     libqdMetaData.system \
     libvulkan \
