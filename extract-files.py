1964c59d92328dc7420f6364c32d950ca3d8da92
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-11 00:50:39 -0400

    msm8998-common: Migrate to libqsap_sdk in hardware/motorola
    Change-Id: I335b4e5e457732d2e05c3c21aad41354bba7210f

--- extract-files.py
+++ extract-files.py
@@ -110,8 +110,6 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .replace_needed('libhidltransport.so', 'libhidlbase.so')
         .remove_needed('libhwbinder.so'),
-    'vendor/lib64/libmdmcutback.so': blob_fixup()
-        .replace_needed('libqsap_sdk.so', 'libqsap_shim.so'),
     'vendor/lib/libmot_gpu_mapper.so': blob_fixup()
         .add_needed('libgui_shim_vendor.so'),
     'vendor/lib64/libmotext_inf.so': blob_fixup()
