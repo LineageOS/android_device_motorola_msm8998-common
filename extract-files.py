7928277580d48c5ebf56bad1b4009b8ca6d8b45e
Marc Bourgoin <themard85@gmail.com>
2025-05-07 18:49:15 -0600

    msm8998-common: Update extract-files.py from template
    Change-Id: I289cbbdd21bc7223ed5e44a77d377634d9e6ab26

--- extract-files.py
+++ extract-files.py
@@ -14,13 +14,9 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
+    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
-    lib_fixup_remove_arch_suffix,
-    lib_fixup_vendorcompat,
-    lib_fixups_user_type,
-    libs_clang_rt_ubsan,
-    libs_proto_3_9_1,
 )
 from extract_utils.main import (
     ExtractUtils,
